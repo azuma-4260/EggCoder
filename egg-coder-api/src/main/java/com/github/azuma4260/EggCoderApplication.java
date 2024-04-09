9bbe6c989313be3a273e207dff32aeac46d1bbdb
azuma <azmax0624@outlook.jp>
2024-04-09 16:50:20 +0900

    feat: Initialize Spring Boot application for EggCoder

---
+++ egg-coder-api/src/main/java/com/github/azuma4260/EggCoderApplication.java
@@ -0,0 +1,11 @@
+package com.github.azuma4260;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggCoderApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EggCoderApplication.class, args);
+}
+}
